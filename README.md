a061d0be7406d8e85ac12706fa75267266e265f1
sercanmar <sercanmar@alu.edu.gva.es>
2023-05-10 19:43:03 +0200

    v2

--- README.md
+++ README.md
@@ -8,3 +8,4 @@ Entorns de Desenrotllament
 
 Programa exemple per activitat de Github amb Python
 
+10/05/2023 19:41: Añadida nueva función para vaciar todos los cubos.
--- cubosagua.py
+++ cubosagua.py
@@ -63,6 +63,7 @@ class juegoCubos:
         print('  (L)lenar un cubo')
         print('  (V)aciar un cubo')
         print('  (M)over el agua de un cubo a otro')
+        print('  (B)uidar tots els poals')
         print('  (S)alir')
 
         while True:
@@ -71,7 +72,7 @@ class juegoCubos:
                 print('Gracias por jugar!')
                 sys.exit()
 
-            if move in ('L', 'V', 'M'):
+            if move in ('L', 'V', 'M', 'B'):
                 return move
 
     def selecCubo(self, mensaje):
@@ -104,6 +105,11 @@ class juegoCubos:
         self.aguaEnCubos[cuboDestino] += cantidadAMover
         self.pasos += 1
 
+    def VaciarTodos(self):
+        for cubo in self.aguaEnCubos:
+            self.aguaEnCubos[cubo] = 0
+        self.pasos += 1
+
     def jugar(self):
         self.mostrarEstadoCubos()
         while True:
@@ -118,6 +124,9 @@ class juegoCubos:
                 cuboOrigen = self.selecCubo('Selecciona el cubo ORIGEN 8, 5, 3 o SALIR:')
                 cuboDestino = self.selecCubo('Selecciona el cubo DESTINO 8, 5, 3 o SALIR:')
                 self.moverCubo(cuboOrigen, cuboDestino)
+            elif opcion == "B":
+                self.VaciarTodos()
+
             self.mostrarEstadoCubos()
             self.checkObjetivo()
 
